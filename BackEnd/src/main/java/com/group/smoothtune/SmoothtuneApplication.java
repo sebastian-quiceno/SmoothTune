f52995d300ac0960e1cd0c3e58c559d25bdb980c
sebastian-quiceno <sebastian.quicenol@upb.edu.co>
2026-02-21 10:06:15 -0500

    Creacion de entorno de desarrollo

---
+++ BackEnd/src/main/java/com/group/smoothtune/SmoothtuneApplication.java
@@ -0,0 +1,13 @@
+package com.group.smoothtune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoothtuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmoothtuneApplication.class, args);
+	}
+
+}
